09585526062de7ade94a771091848f9359948a1d
André Malpica <32719793+Andrefer25@users.noreply.github.com>
2020-12-30 15:52:13 -0300

    Update PresupuestoService.js

--- src/services/PresupuestoService.js
+++ src/services/PresupuestoService.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { presupuestoUrl, uploadPresupuestoUrl } from '../constants/routes';
+import { presupuestoUrl, uploadPresupuestoUrl, egresoFilterUrl } from '../constants/routes';
 import { parsearPresupuesto } from '../helpers/parser';
 import EgresoService from './EgresoService';
 import MediosDePagoService from './MediosDePagoService';
@@ -17,6 +17,26 @@ export default class PresupuestoService {
         const data = await new Promise((resolve, reject) => {
             const result = {};
             Axios.get(`${presupuestoUrl}/${this.entidadId}`).then(presupuestos => {
+                result.presupuestos = presupuestos.data;
+                return result;
+            }).then(result => {
+                this.monedas.getMonedas().then(monedas => {
+                    result.monedas = monedas;
+                    resolve(result);
+                })
+            }).catch(e => {
+                reject(e);
+            })
+        });
+        if (data.presupuestos.length > 0)
+            return parsearPresupuesto(data.presupuestos, data.monedas);
+        else return null;
+    }
+    
+    getFilteredPresupuestos = async () => {
+        const data = await new Promise((resolve, reject) => {
+            const result = {};
+            Axios.get(`${egresoFilterUrl}/${this.entidadId}`).then(presupuestos => {
                 result.presupuestos = presupuestos.data;
                 return result;
             }).then(result => {
